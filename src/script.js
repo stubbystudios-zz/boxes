3f424422fda36868718dca4da208a64f394a32e2
Carey Best <carey@stubbystudios.com>
2020-09-15 21:12:50 -0500

    Refactor

--- src/script.js
+++ src/script.js
@@ -1,18 +1,23 @@
-var box, blackBoxId, newBlackBox, blackBoxActive, blackBoxIdNum;
+var box, blackBoxId, newBlackBox, blackBoxActive, blackBoxIdNum, boxCount;
 
 box = '<div class="box"></div>';
 
-for (var i = 0; i < 9; i++) {
+boxCount = 9;
+
+for (var i = 0; i < boxCount; i++) {
   document.querySelector('.container').insertAdjacentHTML('beforeend', '<div class="box" id="box_' + i + '"></div>');
 }
 
-init();
+document.getElementById('box_4').classList.add('black-box');
 
 document.querySelector('.up-btn').addEventListener('click', function () {
   blackBoxFunction();
 
+  // Catch if the box moves above the top row 
   if (blackBoxIdNum > 2) {
     blackBoxActive.classList.remove('black-box');
+
+    // Mark the active box and move it up one row
     newBlackBox = blackBoxIdNum - 3;
 
     document.getElementById('box_' + newBlackBox).classList.add('black-box');
@@ -24,10 +29,12 @@ document.querySelector('.up-btn').addEventListener('click', function () {
 document.querySelector('.down-btn').addEventListener('click', function () {
   blackBoxFunction();
 
-  if (blackBoxIdNum < 6) {
+  // Catch if the box moves below the bottom row 
+  if (blackBoxIdNum < boxCount - 2) {
     blackBoxActive.classList.remove('black-box');
     newBlackBox = blackBoxIdNum + 3;
 
+    // Mark the active box and move it down one row
     document.getElementById('box_' + newBlackBox).classList.add('black-box');
   } else {
     error();
@@ -37,6 +44,7 @@ document.querySelector('.down-btn').addEventListener('click', function () {
 document.querySelector('.left-btn').addEventListener('click', function () {
   blackBoxFunction();
 
+  // Catch if the box moves left of the left column 
   if (
     blackBoxIdNum !== 0
     && blackBoxIdNum !== 3
@@ -45,6 +53,7 @@ document.querySelector('.left-btn').addEventListener('click', function () {
     blackBoxActive.classList.remove('black-box');
     newBlackBox = blackBoxIdNum - 1;
 
+    // Mark the active box and move it left one column
     document.getElementById('box_' + newBlackBox).classList.add('black-box');
   } else {
     error();
@@ -54,12 +63,15 @@ document.querySelector('.left-btn').addEventListener('click', function () {
 document.querySelector('.right-btn').addEventListener('click', function () {
   blackBoxFunction();
 
+  // Catch if the box moves right of the right column
   if (
     blackBoxIdNum !== 2
     && blackBoxIdNum !== 5
     && blackBoxIdNum !== 8
   ) {
     blackBoxActive.classList.remove('black-box');
+
+    // Mark the active box and move it left one column
     newBlackBox = blackBoxIdNum + 1;
 
     document.getElementById('box_' + newBlackBox).classList.add('black-box');
@@ -75,14 +87,14 @@ function blackBoxFunction() {
 }
 
 function error() {
-  document.querySelector('body').insertAdjacentHTML('afterbegin', '<div class="overlay"></div><div class="error"><p>You have reached the edge.</p></div>');
+  // Error message
+  document.querySelector('body').insertAdjacentHTML('afterbegin', '<div class="overlay"></div><section class="error"><p>You have reached the edge.</p><i class="close"></i></section>');
 
-  document.querySelector('.overlay').addEventListener('click', function () {
-    document.querySelector('.error').remove();
-    document.querySelector('.overlay').remove();
-  });
+  document.querySelector('.overlay').addEventListener('click', closeOverlay);
+  document.querySelector('.close').addEventListener('click', closeOverlay);
 }
 
-function init() {
-  document.getElementById('box_4').classList.add('black-box');
+function closeOverlay() {
+  document.querySelector('.error').remove();
+  document.querySelector('.overlay').remove();
 }
